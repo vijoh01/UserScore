baf2deff6c1a5a69fa1b3cdbe8830b288ad1379a
Ludwig Magnusson <ludwig@mediatool.com>
2017-09-22 09:45:16 +0200

    Update initial app instructions

--- lib/index.jsx
+++ lib/index.jsx
@@ -21,30 +21,28 @@ export default class Main extends React.Component {
             />
           </MTColumn>
           <MTColumn width={ 75 } offset={ 5 }>
-            <MTRow>
-              <MTColumn>
-                <h2>Initial site</h2>
-                <p>
-                  Drop the excel file "scores.xlsx" that you will find in this repo in the area to the left and watch the log output in the console.
-                  We hope this is enough to get you started with the import.
-                </p>
-              </MTColumn>
-            </MTRow>
-            <MTRow>
-              <MTColumn>
-                <h2>Explaining the grid</h2>
-                <p>
-                  In the Mediatool grid you can use MTRow and MTColumn to structure your graphical components.
-                  This is basically what you need to know:
-                </p>
-                <ul>
-                  <li>Each row will be located beneath the previous one</li>
-                  <li>Columns in one row will stretch to the width of the entire parent component and they will have the same size unless you provide them with a with property</li>
-                  <li>If you set the widht you do so in percent</li>
-                </ul>
-                <p>You can also nest these components like this row is nested inside a column.</p>
-              </MTColumn>
-            </MTRow>
+            <div>
+              <h2>Initial site</h2>
+              <p>
+                Drop the excel file "scores.xlsx" that you will find in this repo in the area to the left and watch the log output in the console.
+                We hope this is enough to get you started with the import.
+              </p>
+            </div>
+            <div>
+              <h2>Explaining the grid</h2>
+              <p>
+                In the Mediatool grid you can use MTRow and MTColumn to structure your graphical components.
+                This is basically what you need to know:
+              </p>
+              <ul>
+                <li>Each row will be located beneath the previous one</li>
+                <li>Columns in one row will stretch to the width of the entire parent component and they will have the same size unless you provide them with a with property</li>
+                <li>If you set the width you do so in percent</li>
+                <li>You can also use offset to create space betweeen your columns</li>
+                <li>The total width and offset of the columns in a row should equal 100</li>
+                <li>It is also possible to nest rows and columns within each other</li>
+              </ul>
+            </div>
           </MTColumn>
         </MTRow>
       </div>
